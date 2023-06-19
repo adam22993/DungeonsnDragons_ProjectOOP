ea3a7c3a9f959105a2bf1f44ac47e101ac9f8db0
Adam Simkin <adam.simkin@outlook.com>
2023-06-19 13:44:40 +0300

    added argument management

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,37 @@ import GameBoard.GameBoard;
 import GameBoard.Levels.Level;
 import GameBoard.Levels.LevelLoader;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No levels directory specified.");
+            return;
+        } else if (args.length > 1) {
+            System.out.println("Too many arguments.");
+            return;
+        } else if (!checkIfFolderContainsLevelsTXT(args[0])) {
+            System.out.println("The specified folder does not contain any .txt files.");
+            return;
+        } else {
+            System.out.println("Loading levels from " + args[0] + "...");
+            ControlLayer controlLayer = new ControlLayer(new GameBoard(new LevelLoader(args[0])));
+        }
+    }
 
-        ControlLayer controlLayer = new ControlLayer(new GameBoard(new LevelLoader("src/GameBoard/Levels")));
-//        LevelLoader levelLoader = new LevelLoader("src\\GameBoard\\Levels");
+
+
+    public static boolean checkIfFolderContainsLevelsTXT(String directory) {
+        File folder = new File(directory);
+        File[] files = folder.listFiles();
+        if (files != null && files.length != 0) {
+            for (File file : files) {
+                if (file.getName().endsWith(".txt")) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
